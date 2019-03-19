4ab10d226ef92c2eda7dbc36b2a7f7484f06cb7d
Ubuntu <allen224n@224n-allenNLP.fcsn2n43yskehp5sscaifxta5b.bx.internal.cloudapp.net>
2019-03-19 11:47:45 +0000

    evaluated bert

--- AllenBert/allen_train_bert.py
+++ AllenBert/allen_train_bert.py
@@ -45,9 +45,9 @@ config = Config(
 def bert_tokenizer(s: str):
     return token_indexer.wordpiece_tokenizer(s)[:config.max_seq_len - 2]
 
-def predict(vocab2=None):
+def predict(vocab2):
 	bert_token_indexer = PretrainedBertIndexer(
-	    pretrained_model="bert-base-uncased",
+	    pretrained_model="bert-large-uncased",
 	    max_pieces=config.max_seq_len,
 	    do_lowercase=True,
 	)
@@ -56,10 +56,10 @@ def predict(vocab2=None):
 		token_indexers={'tokens':bert_token_indexer}
 	)	
 
-	train_dataset, test_dataset, dev_dataset = (reader.read(DATA_ROOT + "/" + fname) for fname in ["train_sm.txt", "test_sm.txt", "val_sm.txt"])
+	train_dataset, test_dataset, dev_dataset = (reader.read(DATA_ROOT + "/" + fname) for fname in ["train_all.txt", "test_all.txt", "val_all.txt"])
 
 	bert_embedder = PretrainedBertEmbedder(
-	         pretrained_model='bert-base-uncased',
+	         pretrained_model='bert-large-uncased',
 	         top_layer_only=True, # conserve memory
 	)
 	word_embeddings: TextFieldEmbedder = BasicTextFieldEmbedder({"tokens": bert_embedder},
@@ -77,10 +77,14 @@ def predict(vocab2=None):
 	    def get_output_dim(self) -> int:
 	        return BERT_DIM
 	        
-	if not vocab2: 
-		vocab2 = Vocabulary.from_files("./bert_vocabulary")
+	# if not vocab2: 
+	# 	vocab2 = Vocabulary.from_files("./bert_vocabulary")
+
 	bert_encoder = BertSentencePooler(vocab2)
 	model2 = LstmModel(word_embeddings, bert_encoder, vocab2)
+	if USE_GPU: model2.cuda()
+	else: model2
+
 	with open("./bert_model.th", 'rb') as f:
 		model2.load_state_dict(torch.load(f))
 	
@@ -191,8 +195,72 @@ def main():
 	vocab.save_to_files("bert_vocabulary")
 	return vocab
 
+def get_vocab():
+		#Initlizing the embeddings (BERT)
+	bert_token_indexer = PretrainedBertIndexer(
+	    pretrained_model="bert-large-uncased",
+	    max_pieces=config.max_seq_len,
+	    do_lowercase=True,
+	)
+	reader = BertAnalogyDatasetReader(
+		tokenizer=bert_tokenizer,
+		token_indexers={'tokens':bert_token_indexer}
+	)
+
+	train_dataset, test_dataset, dev_dataset = (reader.read(DATA_ROOT + "/" + fname) for fname in ["train_all.txt", "test_all.txt", "val_all.txt"])
+	
+	vocab = Vocabulary.from_instances(train_dataset + test_dataset + dev_dataset)
+
+
+	bert_embedder = PretrainedBertEmbedder(
+	         pretrained_model='bert-large-uncased',
+	         top_layer_only=True, # conserve memory
+	)
+	word_embeddings: TextFieldEmbedder = BasicTextFieldEmbedder({"tokens": bert_embedder},
+	                                                             # we'll be ignoring masks so we'll need to set this to True
+	                                                            allow_unmatched_keys = True)
+
+	BERT_DIM = word_embeddings.get_output_dim()
+
+	class BertSentencePooler(Seq2VecEncoder):
+	    def forward(self, embs: torch.tensor, 
+	                mask: torch.tensor=None) -> torch.tensor:
+	        # extract first token tensor
+	        return embs[:, 0]
+	    
+	    @overrides
+	    def get_output_dim(self) -> int:
+	        return BERT_DIM
+	 
+	#Initializing the model
+	#takes the hidden state at the last time step of the LSTM for every layer as one single output
+	bert_encoder = BertSentencePooler(vocab)
+
+	model = LstmModel(word_embeddings, bert_encoder, vocab)
+	if USE_GPU: model.cuda()
+	else: model
+
+	# Training the model 
+	optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)
+	iterator = BucketIterator(batch_size=32, sorting_keys=[("tokens", "num_tokens")])
+	iterator.index_with(vocab)
+
+	trainer = Trainer(model=model,
+                  optimizer=optimizer,
+                  iterator=iterator,
+                  train_dataset=train_dataset,
+                  validation_dataset=dev_dataset,
+                  patience=10,
+                  cuda_device=0 if USE_GPU else -1,
+                  num_epochs=1)
+
+	# trainer.train()
+
+	return vocab
+
 if __name__ == '__main__':
 	# vocab = main()
-	predict()
+	vocab = get_vocab()
+	predict(vocab)
 	eval_predictions("bert_predictions.txt", DATA_ROOT + "/" + "test_all.txt")
 
